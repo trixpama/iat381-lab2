1f7dec8ede0608e9a528cf4be4fbdf715ec84aa5
trixpama <trixpama@gmail.com>
2015-01-18 23:50:29 -0800

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myappApp
+ * @description
+ * # myappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myappApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngMessages',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
